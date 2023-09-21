4fae1cfb6af649e0f9166d4c758251e4d91eca7b
Dominik-Hillmann <dominik.hillmann@gmx.de>
2023-09-21 21:49:06 +0200

    feat: more options for booking

--- static/js/price-calculator.js
+++ static/js/price-calculator.js
@@ -1,6 +1,7 @@
 const startDateInput = document.querySelector("#start");
 const endDateInput = document.querySelector("#end");
-const numGuestsInput = document.querySelector("#number-people");
+const numAdultsInput = document.querySelector("#num-adults");
+const numChildrenInput = document.querySelector("#num-children");
 const priceOutput = document.querySelector("#price-output");
 
 const updatePrice = () => {
@@ -9,37 +10,63 @@ const updatePrice = () => {
     const [endDateYear, endDateMonth, endDateDay] = endDateInput.value.split("-");
     const endDate = new Date(endDateYear, endDateMonth, endDateDay);
 
-    const numGuests = parseInt(numGuestsInput.value);
-    const basePricePerNight = 100.0;
+    const numAdults = parseInt(numAdultsInput.value);
+    const numChildren = parseInt(numChildrenInput.value);
+    const adultBasePricePerNight = 80.0;
+    const childBasePricePerNight = 20.0;
     const timeDelta = endDate - startDate;
     const numNights = Math.ceil(timeDelta / (24 * 60 * 60 * 1000));
-    const projectedPrice = basePricePerNight * numNights * numGuests;
+    const projectedPrice = numNights 
+        * (adultBasePricePerNight * numAdults 
+        + childBasePricePerNight * numChildren);
+    console.log(projectedPrice)
 
     if (LANGUAGE === "Deutsch") {
         const numNightsOutput = numNights <= 1 ? "einer" : `${numNights}`;
-        const numGuestsOutput = numGuests <= 1 ? "einem" : `${numGuests}`;
+
+        const numAdultsOutput = numAdults <= 1 ? "einem" : `${numAdults}`;
+
+        let numChildrenOutput = numChildren <= 1 ? " und einem Kind" : ` und ${numChildren} Kindern`;
+        numChildrenOutput = numChildren == 0 ? "" : numChildrenOutput;
+
         const nightOutput = numNights > 1 ? "Nächten" : "Nacht";
-        const guestOutput = numGuests > 1 ? "Gästen" : "Gast";
+        const adultOutput = numAdults > 1 ? "volljährigen Gästen" : "volljährigem Gast";
         priceOutput.innerHTML = numNights <= 0 
             ? "" 
-            : `Ein Aufenthalt von ${numNightsOutput} ${nightOutput} mit ${numGuestsOutput} ${guestOutput} kostet voraussichtlich EUR ${projectedPrice}.`;
+            : `Ein Aufenthalt von ${numNightsOutput} ${nightOutput} 
+                mit ${numAdultsOutput} ${adultOutput}${numChildrenOutput} kostet voraussichtlich 
+                EUR ${projectedPrice}.`;
 
     } else if (LANGUAGE === "English") {
         const numNightsOutput = numNights <= 1 ? "one" : `${numNights}`;
-        const numGuestsOutput = numGuests <= 1 ? "a single" : `${numGuests}`;
+
+        const numAdultsOutput = numAdults <= 1 ? "a single" : `${numAdults}`;
+
+        let numChildrenOutput = numChildren <= 1 ? " and one child" : ` and ${numChildren} children`;
+        numChildrenOutput = numChildren == 0 ? "" : numChildrenOutput;
+
         const nightOutput = numNights > 1 ? "nights" : "night";
-        const guestOutput = numGuests > 1 ? "guests" : "guest";
+        const adultOutput = numAdults > 1 ? "adults" : "adult";
         priceOutput.innerHTML = numNights <= 0 
             ? "" 
-            : `A stay of ${numNightsOutput} ${nightOutput} accomodating ${numGuestsOutput} ${guestOutput} is expected to cost EUR ${projectedPrice}.`;
+            : `A stay of ${numNightsOutput} ${nightOutput} 
+                accomodating ${numAdultsOutput} ${adultOutput}${numChildrenOutput} is expected to cost 
+                EUR ${projectedPrice}.`;
     
-    } else console.error(`No translation found for language '${LANGUAGE}'.`);    
+    } else console.error(`No translation found for language '${LANGUAGE}'.`);
+    console.log([
+        startDateInput, 
+        endDateInput, 
+        numAdultsInput,
+        numChildrenInput
+    ])  
 };
 
 [
     startDateInput, 
     endDateInput, 
-    numGuestsInput
+    numAdultsInput,
+    numChildrenInput
 ].forEach(input => input.addEventListener("change", updatePrice));
 
 updatePrice();
