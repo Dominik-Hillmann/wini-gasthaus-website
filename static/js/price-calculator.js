0baa88943863cee1a6aaee155da861351c97a8f6
Dominik-Hillmann <dominik.hillmann@gmx.de>
2024-01-02 23:08:39 +0100

    feat: adjust backend inteface

--- static/js/price-calculator.js
+++ static/js/price-calculator.js
@@ -2,6 +2,7 @@ const startDateInput = document.querySelector("#start");
 const endDateInput = document.querySelector("#end");
 const numAdultsInput = document.querySelector("#num-adults");
 const numChildrenInput = document.querySelector("#num-children");
+const numPetsInput = document.querySelector("#num-pets")
 const priceOutput = document.querySelector("#price-output");
 
 const updatePrice = () => {
@@ -9,57 +10,83 @@ const updatePrice = () => {
     const startDate = new Date(startDateYear, startDateMonth, startDateDay);
     const [endDateYear, endDateMonth, endDateDay] = endDateInput.value.split("-");
     const endDate = new Date(endDateYear, endDateMonth, endDateDay);
+    const timeDelta = endDate - startDate;
+    const numNights = Math.ceil(timeDelta / (24 * 60 * 60 * 1000));
 
     const numAdults = parseInt(numAdultsInput.value);
     const numChildren = parseInt(numChildrenInput.value);
-    const adultBasePricePerNight = 80.0;
-    const childBasePricePerNight = 20.0;
-    const timeDelta = endDate - startDate;
-    const numNights = Math.ceil(timeDelta / (24 * 60 * 60 * 1000));
-    const projectedPrice = numNights 
-        * (adultBasePricePerNight * numAdults 
-        + childBasePricePerNight * numChildren);
+    const numPets = parseInt(numPetsInput.value);
 
-    if (LANGUAGE === "Deutsch") {
-        const numNightsOutput = numNights <= 1 ? "einer" : `${numNights}`;
+    const baseUrl = "https://gasthaus-wini.com/api/v1/price";
+    //const baseUrl = "http://localhost:8080/api/v1/price"
+    const queryParams = new URLSearchParams({
+        startDate: `${startDateInput.value}`,
+        endDate: `${endDateInput.value}`,
+        numAdults: `${numAdults}`,
+        numChildren: `${numChildren}`,
+        numPets: `${numPets}`
+    });
 
-        const numAdultsOutput = numAdults <= 1 ? "einem" : `${numAdults}`;
+    const insertPriceOrError = price => {
+        if (LANGUAGE === "Deutsch") {
+            const numNightsOutput = numNights <= 1 ? "einer" : `${numNights}`;
 
-        let numChildrenOutput = numChildren <= 1 ? " und einem Kind" : ` und ${numChildren} Kindern`;
-        numChildrenOutput = numChildren == 0 ? "" : numChildrenOutput;
+            const numAdultsOutput = numAdults <= 1 ? "einem" : `${numAdults}`;
 
-        const nightOutput = numNights > 1 ? "Nächten" : "Nacht";
-        const adultOutput = numAdults > 1 ? "volljährigen Gästen" : "volljährigem Gast";
-        priceOutput.innerHTML = numNights <= 0 
-            ? "" 
-            : `Ein Aufenthalt von ${numNightsOutput} ${nightOutput} 
-                mit ${numAdultsOutput} ${adultOutput}${numChildrenOutput} kostet voraussichtlich 
-                EUR ${projectedPrice}.`;
+            let numChildrenOutput = numChildren <= 1 ? " und einem Kind" : ` und ${numChildren} Kindern`;
+            numChildrenOutput = numChildren === 0 ? "" : numChildrenOutput;
 
-    } else if (LANGUAGE === "English") {
-        const numNightsOutput = numNights <= 1 ? "one" : `${numNights}`;
+            let numPetsOutput = numPets <= 1 ? ", einem Haustier" : `, ${numPets} Haustieren`;
+            numPetsOutput = numPets === 0 ? "" : numPetsOutput;
 
-        const numAdultsOutput = numAdults <= 1 ? "a single" : `${numAdults}`;
+            const nightOutput = numNights > 1 ? "Nächten" : "Nacht";
+            const adultOutput = numAdults > 1 ? "volljährigen Gästen" : "volljährigem Gast";
+            priceOutput.innerHTML = numNights <= 0
+                ? ""
+                : `Ein Aufenthalt von ${numNightsOutput} ${nightOutput} 
+            mit ${numAdultsOutput} ${adultOutput}${numPetsOutput}${numChildrenOutput} kostet voraussichtlich 
+            EUR ${price.toFixed(2)}.`;
 
-        let numChildrenOutput = numChildren <= 1 ? " and one child" : ` and ${numChildren} children`;
-        numChildrenOutput = numChildren == 0 ? "" : numChildrenOutput;
+        } else if (LANGUAGE === "English") {
+            const numNightsOutput = numNights <= 1 ? "one" : `${numNights}`;
 
-        const nightOutput = numNights > 1 ? "nights" : "night";
-        const adultOutput = numAdults > 1 ? "adults" : "adult";
-        priceOutput.innerHTML = numNights <= 0 
-            ? "" 
-            : `A stay of ${numNightsOutput} ${nightOutput} 
-                accomodating ${numAdultsOutput} ${adultOutput}${numChildrenOutput} is expected to cost 
-                EUR ${projectedPrice}.`;
-    
-    } else console.error(`No translation found for language '${LANGUAGE}'.`);
+            const numAdultsOutput = numAdults <= 1 ? "a single" : `${numAdults}`;
+
+            let numChildrenOutput = numChildren <= 1 ? " and one child" : ` and ${numChildren} children`;
+            numChildrenOutput = numChildren === 0 ? "" : numChildrenOutput;
+
+            let numPetsOutput = numPets <= 1 ? ", a single pet" : `, ${numPets} pets`;
+            numPetsOutput = numPets === 0 ? "" : numPetsOutput;
+
+            const nightOutput = numNights > 1 ? "nights" : "night";
+            const adultOutput = numAdults > 1 ? "adults" : "adult";
+            priceOutput.innerHTML = numNights <= 0
+                ? ""
+                : `A stay of ${numNightsOutput} ${nightOutput} 
+            accommodating ${numAdultsOutput} ${adultOutput}${numPetsOutput}${numChildrenOutput} is expected to cost 
+            EUR ${price.toFixed(2)}.`;
+
+        } else console.error(`No translation found for language '${LANGUAGE}'.`);
+    }
+
+    fetch(`${baseUrl}?${queryParams.toString()}`, { method: "GET" })
+        .then(response => response.json())
+        .then(data => {
+            if (data.price) {
+                insertPriceOrError(data.price);
+            } else {
+                priceOutput.innerHTML = LANGUAGE === "Deutsch" ? data["message-german"] : data["message-english"];
+            }
+        })
+        .catch(error => console.error(error));
 };
 
 [
     startDateInput, 
     endDateInput, 
     numAdultsInput,
-    numChildrenInput
+    numChildrenInput,
+    numPetsInput
 ].forEach(input => input.addEventListener("change", updatePrice));
 
 updatePrice();
